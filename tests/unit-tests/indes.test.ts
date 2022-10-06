fd6f8fb7a05a3993c14254313880600c063c3bf6
Barder60 <kaderate.yamamoto@hotmail.fr>
2022-10-06 14:39:58 +0200

    fix(CI): add --forceExit

--- tests/unit-tests/indes.test.ts
+++ tests/unit-tests/indes.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
 import { calculate } from '../../src/index';
 
-describe('calculate module', () => {
+describe('calculate function', () => {
   test('should return 1', () => {
     expect(calculate()).toBe(1);
   });
