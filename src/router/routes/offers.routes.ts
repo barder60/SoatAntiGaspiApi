dec13e3bc53dcc0fe8095ec450d15a4d6fbfe05f
formation <benamoyoussef@gmail.com>
2022-10-17 11:52:03 +0200

    update offers.routes, fix swagger

--- src/router/routes/offers.routes.ts
+++ src/router/routes/offers.routes.ts
@@ -1,13 +1,55 @@
 import express from "express";
 const router = express.Router();
 import getConnection from '../../models';
+import moment from 'moment';
+
 
 const db = getConnection();
+const {Offer} = db
 
 
-router.get('/offers', (req, res) => {
-    db.Offer.findAll().then((result: object) => res.json(result)).catch((err: object) => console.error(err));
+router.get('/offers', async(req, res) => {
+    try {
+        
+        const offersGetted = await Offer.findAll({
+            attributes: ["id", "title", "description"],
+            rejectOnEmpty: true
+        })
+
+        res.status(200).json({ offersGetted })
+
+    } catch (err: any) {
+        if (err.name === 'SequelizeEmptyResultError')
+            return res.status(404).json({ message: 'The requested resource does not exist.' })
+    }
 })
+
+
+router.post('/offers', async(req, res) => {
+    try {
+
+        const { body: newOffer}  = req
+        let newBody = {
+            ...newOffer
+        }
+
+        newBody.expiration = new Date(newOffer.expiration)
+        newBody.availability = new Date(newOffer.availability)
+        
+        const offersCreated = await Offer.create({
+            ...newBody
+        })
+
+        res.status(201).json({ offersCreated })
+
+    } catch (err: any) {
+        if (err.name === 'SequelizeValidationError')
+            return res.status(404).json({ message: err.message })
+        if (err.name === 'SequelizeEmptyResultError')
+            return res.status(404).json({ message: 'The requested resource does not exist.' })
+    }
+})
+
 
 
 export default router;
