b4736f3b335f3ec31aedd1957bcfaa11ab3ce4cf
formation <benamoyoussef@gmail.com>
2022-10-17 12:17:47 +0200

    add get offer /:id

--- src/router/routes/offers.routes.ts
+++ src/router/routes/offers.routes.ts
@@ -24,6 +24,21 @@ router.get('/offers', async(req, res) => {
     }
 })
 
+router.get('/offer/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        const offersGetted = await Offer.findByPk(id, {
+            rejectOnEmpty: true
+        })
+
+        res.status(200).json({ offersGetted })
+
+    } catch (err: any) {
+        if (err.name === 'SequelizeEmptyResultError')
+            return res.status(404).json({ message: 'The requested resource does not exist.' })
+    }
+})
+
 
 router.post('/offers', async(req, res) => {
     try {
